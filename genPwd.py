435429380b9d661e217133f9bb80883bea840985
Yngve Levinsen <yngve@pm.me>
2019-01-22 11:41:14 +0100

    using secrets instead of random (requires python3.6) resorting settings renaming the file names for generated lists

--- genPwd.py
+++ genPwd.py
@@ -1,40 +1,43 @@
-import os, struct, sys, random, string
+import os, struct, sys, secrets, string
 
 # Settings
 # The aspell dictionary to select from:
 lang        = 'is'
-# Take out any words from dictionary containing these characters:
-word_filter = './-'
 # Number of words in the password
-nWords      = 2
+nWords      = 3
 # Probability for each character to be upper case:
 probUpper   = 0.5
 # Number of digits between each word:
 nDigits   = 2
 # Number of special characters after each number:
 nSpecials = 1
-# Only pick from words with at least this length:
-wlen_min  = 5
-# Only pick from words with at most this length:
-wlen_max  = 8
 
 # Minimum password length:
 pwd_minlen = 10
 # Maximum password length:
 pwd_maxlen = 30
+
+# The following settings defines the unfiltered - filtered list
+# Take out any words from dictionary containing these characters:
+word_filter = './-'
+# Only pick from words with at least this length:
+wlen_min  = 5
+# Only pick from words with at most this length:
+wlen_max  = 8
+
 # End of settings
 
 # ------
 
 # First generate a full list of words from aspell-nb:
-if not os.path.isfile('my.dict'):
-    cmd = 'aspell -d {0} dump master | aspell -l {0} expand > my.dict'.format(lang)
+if not os.path.isfile('unfiltered.txt'):
+    cmd = 'aspell -d {0} dump master | aspell -l {0} expand > unfiltered.txt'.format(lang)
     os.system(cmd)
 
 # Remove words we don't want included..
-if not os.path.isfile('my2.dict'):
-    fo = open( 'my2.dict', 'w')
-    for l in open('my.dict','r'):
+if not os.path.isfile('filtered.txt'):
+    fo = open( 'filtered.txt', 'w')
+    for l in open('unfiltered.txt','r'):
         ls = l.strip()
         # Remove all very short and very long words:
         if len(ls) < wlen_min or len(ls) > wlen_max:
@@ -47,21 +50,18 @@ if not os.path.isfile('my2.dict'):
             continue
         fo.write(l)
 
-with open('my2.dict') as f:
+with open('filtered.txt') as f:
     lines = f.read().splitlines()
 
 maxN = len(lines)
 
-# Needed for selection of random digit:
-digits = [str(i) for i in range(10)]
-rnd = random.SystemRandom()
+rnd = secrets.SystemRandom()
 
 def gen_pwd():
     pwd = ''
     
     for n in range(nWords):
-        ran = rnd.randint(0, maxN)
-        word = list(lines[ran])
+        word = list( rnd.choice(lines))
         for j in range(len(word)):
             if rnd.uniform(0.0, 1.0) < probUpper:
                 word[j] = word[j].upper()
